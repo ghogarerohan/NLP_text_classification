d6bb354aa38892462daf1e2871570f0193d79cb5
Rohan Ghogare <ghogare.rohan@gmail.com>
2024-04-10 15:36:49 +0530

    setup completed

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="hate-speech-classification",
+    version="0.0.1",
+    author="Rohan",
+    author_email="ghogare.rohan@gmail.com",
+    packages=find_packages(),
+    install_requires=[],
+)
